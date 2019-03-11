e697c5f651984343e3a9126fb282848e6dfc45b6
Simon <sdat2@cam.ac.uk>
2019-03-11 16:23:43 +0000

    everything working now

--- ex2/FourthOMatrix.py
+++ ex2/FourthOMatrix.py
@@ -1,4 +1,4 @@
-#Exammple 4
+#Fourth Order RK Damped Harmonic
 # Solve y'''(t)+4y''+6y'+4=gamma(t), Euler method,
 #so Hacky (np arrays are disgusting, every time you slice them they go flat)
 import numpy as np
@@ -7,9 +7,14 @@ import matplotlib.pyplot as plt
 q0 = np.array([[0],[-1],[0]])                  # Initial Condition
 h=0.05;                   # Time step
 tmax = 30;
+
+#The matrix transformation
 A = np.array([[0,1,0],[0,0,1],[-4,-6,-4]])
 B =np.array([[0],[0],[1]])
-t = np.linspace(0,tmax,num=int(5/h))
+
+t = np.linspace(0,tmax,num=int(tmax/h))
+
+# The exact solution that I worked out.
 yexact = [1/4 + np.exp(-x)*(np.cos(x)-5/2*np.sin(x))-5/4*np.exp(-2*x) for x in t]    # Exact solution
 
 qstar = np.zeros((3,len(t)));
@@ -18,6 +23,8 @@ qstar[0][0]=q0[0][0]; qstar[1][0]=q0[1][0]; qstar[2][0]=q0[2][0];
 
 k1 = np.zeros((3,1))
 q1 = np.zeros((3,1))
+
+###### Manually implementing Runge Kutta 4 Algorithm ######
 for i in range(len(t)-1):
     k1 = A.dot(qstar[:,i]).reshape(3,1)+B  # Approx for y gives approx for deriv
     q1 = (qstar[:,i].reshape(3,1) +k1*h/2)
@@ -26,13 +33,14 @@ for i in range(len(t)-1):
     k3 = A.dot(q2)+B
     q3 = (qstar[:,i].reshape(3,1)+k3*h)
     k4 = A.dot(q3)+B
-    qstar[:,i+1]= (qstar[:,i].reshape(3,1)+(k1+2*k2+2*k3+k4)*(h/6)).reshape(1,3)
+    qstar[:,i+1]= (qstar[:,i].reshape(3,1)+(k1+2*k2+2*k3+k4)*(h)/6).reshape(1,3)
 
-plt.plot(t,yexact,color='black', label= 'Exact')
+#### Plot this.
+
 plt.plot(t,qstar[0,:],color='blue', marker='+', label='$4^{th}$ order Runge Kutta')
+plt.plot(t,yexact,color='black', label= 'Exact')
 plt.xlabel('Time (s)')
-plt.ylabel('y')
+plt.ylabel('Displacement of Pendulum, y (m)')
 plt.legend()
 plt.savefig('FourthOMatrix.pdf')
 plt.clf()
-#legend('Exact','Approximate');
--- ex2/SOMatrix.py
+++ ex2/SOMatrix.py
@@ -1,4 +1,4 @@
-#Exammple 4
+#Second Order Runge Kutta Damped Harmonic
 # Solve y'''(t)+4y''+6y'+4=gamma(t), Euler method,
 #so Hacky (np arrays are disgusting, every time you slice them they go flat)
 import numpy as np
@@ -6,28 +6,37 @@ import matplotlib.pyplot as plt
 
 q0 = np.array([[0],[-1],[0]])                  # Initial Condition
 h=0.05;                   # Time step
+tmax = 5
+##### Matrix
 A = np.array([[0,1,0],[0,0,1],[-4,-6,-4]])
 B =np.array([[0],[0],[1]])
-t = np.linspace(0,5,num=int(5/h))
+
+
+t = np.linspace(0,tmax,num=int(tmax/h))
+
+# The exact solution that I worked out.
 yexact = [1/4 + np.exp(-x)*(np.cos(x)-5/2*np.sin(x))-5/4*np.exp(-2*x) for x in t]    # Exact solution
+
+
 qstar = np.zeros((3,len(t)));
 qstar[0][0]=q0[0][0];qstar[1][0]=q0[1][0];qstar[2][0]=q0[2][0];
  # Initial condition gives solution at t=0.
 k1 = np.zeros((3,1))
 q1 = np.zeros((3,1))
 
+
+####### Manually implementing Runge Kutta 2 Algorithm #####
 for i in range(len(t)-1):
 	k1 = A.dot(qstar[:,i]).reshape(3,1)+B  # Approx for y gives approx for deriv
 	q1 = (qstar[:,i].reshape(3,1) +k1*h/2)
 	k2 = A.dot(q1)+B
 	qstar[:,i+1]= (qstar[:,i].reshape(3,1)+k2*(h)).reshape(1,3)         # Intermediate value
 
-	#k2 = q1*(1-2*(t[i]+h/2))                    # Approx deriv at intermediate value.
-	#ystar[i+1] = ystar[i] + k1*h; # Approximate solution at next value of y
-plt.plot(t,yexact,color='black')
-plt.plot(t,qstar[0,:],color='blue', marker='+')
+
+plt.plot(t,qstar[0,:],color='blue', marker='+',label='$2^{nd}$ order Runge Kutta')
+plt.plot(t,yexact,color='black',label= 'Exact')
 plt.xlabel('Time (s)')
-plt.ylabel('y')
+plt.ylabel('Displacement of Pendulum, y (m)')
+plt.legend()
 plt.savefig('SOMatrix.pdf')
 plt.clf()
-#legend('Exact','Approximate');
