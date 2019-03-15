d6b738fede8c7ae67a66e3f29e188dd2d66d7bc4
Simon <sdat2@cam.ac.uk>
2019-03-15 15:22:46 +0000

    all ok

--- ex2/main/3_SupTask.py
+++ ex2/main/3_SupTask.py
@@ -13,9 +13,29 @@ oscillations=170
 graph_os = oscillations
 sample_os = 20
 
-def Small_Del_Comp():
+def Small_Del_Comp( f= 1.2, oscillations = 170):
 	apparent_period,DispA,VelA,TA = Super_Pen(init=0.20)
 	apparent_period,DispB,VelB,TB = Super_Pen(init=0.20001)
+	displacements = []
+	velocities = []
+	init_d = [0.20, 0.2001]
+	for init in init_d:
+		p, y, y_dot, y_t, E, E_t = Super_Pen(init= init_d,h=h, \
+		                                     oscillations= oscillations,\
+											 omega_d=omega_d,\
+											 F = f,\
+											 z = z)
+		displacements.append(y)
+		velocities.append(y_dot)
+
+	for i in range(len(displacements)):
+		plt.plot(y_t, displacements[i], label='Initial Displacement ='+str(init_d])+'radians')
+		plt.legend()
+	plt.xlabel('Time (seconds)')
+	plt.ylabel('Displacement (radians)')
+	plt.title('Undriven Initial Oscillations with Varied Damping')
+	plt.savefig('G_Initial_Oscillations_Varied_Damping.pdf')
+	plt.clf()
 
 if True:
 	plt.plot(TA,DispA,color='blue')
