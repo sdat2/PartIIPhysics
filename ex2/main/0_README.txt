9310c89003754f58ec8af7bca38d67f9b43afbd0
Simon <sdat2@cam.ac.uk>
2019-03-15 15:44:25 +0000

    oh sod it

--- ex2/main/0_README.txt
+++ ex2/main/0_README.txt
@@ -1,6 +1,6 @@
 
-Script 1_Task1Pendulum.py Runs three sections of code.
+Script 1_Task1Pendulum.py produces Figures A-C when run.
 
-Script 2_Task2Pendulum.py 
+Script 2_Task2Pendulum.py produces Figures D-G when run.
 
-Script 3_SupTask.py
+Script 3_SupTask.py produces Figures H-I when run.
--- ex2/main/3_SupTask.py
+++ ex2/main/3_SupTask.py
@@ -13,14 +13,12 @@ oscillations=170
 graph_os = oscillations
 sample_os = 20
 
-def Small_Del_Comp( f= 1.2, oscillations = 170):
-	apparent_period,DispA,VelA,TA = Super_Pen(init=0.20)
-	apparent_period,DispB,VelB,TB = Super_Pen(init=0.20001)
+def Small_Del_Comp( f= 1.2, oscillations = 170, z=0.5):
 	displacements = []
 	velocities = []
 	init_d = [0.20, 0.2001]
 	for init in init_d:
-		p, y, y_dot, y_t, E, E_t = Super_Pen(init= init_d,h=h, \
+		p, y, y_dot, y_t, E, E_t = Super_Pen(init=init,h=h, \
 		                                     oscillations= oscillations,\
 											 omega_d=omega_d,\
 											 F = f,\
@@ -29,26 +27,20 @@ def Small_Del_Comp( f= 1.2, oscillations = 170):
 		velocities.append(y_dot)
 
 	for i in range(len(displacements)):
-		plt.plot(y_t, displacements[i], label='Initial Displacement ='+str(init_d])+'radians')
+		plt.plot(y_t, displacements[i], label='Initial Displacement ='+str(init_d[i])+' radians')
 		plt.legend()
 	plt.xlabel('Time (seconds)')
 	plt.ylabel('Displacement (radians)')
-	plt.title('Undriven Initial Oscillations with Varied Damping')
-	plt.savefig('G_Initial_Oscillations_Varied_Damping.pdf')
+	plt.title('Driven Oscillations for 170 Oscilliations')
+	plt.savefig('H_Small_Init_Diff_Change_Displacements.pdf')
+	plt.clf()
+	for i in range(len(velocities)):
+		plt.plot(y_t, velocities[i], label='Initial Displacement ='+str(init_d[i])+' radians')
+		plt.legend()
+	plt.xlabel('Time (seconds)')
+	plt.ylabel('Angular Velocity (radians per second)')
+	plt.title('Driven Oscillations for 170 Oscilliations')
+	plt.savefig('I_Small_Init_Diff_Change_Velocities.pdf')
 	plt.clf()
 
-if True:
-	plt.plot(TA,DispA,color='blue')
-	plt.plot(TB,DispB,color='orange')
-	plt.xlabel('Time (s)')
-	plt.ylabel('Displacement (radians)')
-	plt.savefig('T3_Displacement.pdf')
-	plt.clf()
-if True:
-	plt.plot(TA,VelA,color='blue')
-	plt.plot(TB,VelB,color='orange')
-	plt.xlabel('Time (s)')
-	plt.ylabel('Angular velocity (rad/s)')
-	plt.savefig('T3_Velocity.pdf')
-
-print('Apparent period of oscillation (when Forcing='+str(F)+') (from sign changes of ang velocity):', apparent_period)
+Small_Del_Comp()
